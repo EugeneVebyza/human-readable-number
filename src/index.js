f46a75b3b736d159719781270c6bb227b6195cbf
EugeneVebyza <36309477+EugeneVebyza@users.noreply.github.com>
2020-03-21 17:09:19 +0400

    unit: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+    var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+  var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+  if(number == 0){ return 'zero';}
+
+	n = ('000' + number).substr(-3).match(/^(\d{1})(\d{2})$/);
+	if (!n) return; var str = '';
+	str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + '' + a[n[1][1]]) + 'hundred ' : '';
+	str += (n[2] != 0) ? ((str != '') ? '' : '') + (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + '' : '';
+	return str.substring(0, str.length - 1);
 }
